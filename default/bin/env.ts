4f485321fb1ea207a8fdcc21f40582e0ced851ec
TShokhrukh <shohruh8945@gmail.com>
2023-05-21 19:57:38 +0500

    remove default

--- default/bin/env.ts
+++
@@ -1,9 +0,0 @@
-type TEnvMode = 'none' | 'development' | 'production' | undefined
-
-export function envMode(): TEnvMode {
-  return (process.env.NODE_ENV as TEnvMode)
-}
-
-export function isDev() {
-  return envMode() === 'development'
-}
