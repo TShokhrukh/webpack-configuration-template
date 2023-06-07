adf4656fba0e97b812f3736366341560593668a9
TShokhrukh <shohruh8945@gmail.com>
2023-06-07 12:36:55 +0500

    update jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   roots: ['<rootDir>/src'],
-  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx,js,jsx}'],
+  setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
   testEnvironment: 'jsdom',
   testEnvironmentOptions: { resources: 'usable' },
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|s[ac]ss)$': 'identity-obj-proxy'
   }
 }
